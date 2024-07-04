8a7299ec0068356a2fba62175d749a1175db3cfd
tqt97 <kutuanonline199@gmail.com>
2024-07-04 15:49:33 +0700

    Polymorphic Relationships

---
+++ database/factories/VideoFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Tag;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\video>
+ */
+class VideoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'url' => $this->faker->url,
+        ];
+    }
+
+    public function tags()
+    {
+        return $this->morphMany(Tag::class, 'taggable');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\Todo;
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Video;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,6 +18,8 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         Todo::factory(10)->create();
+        Post::factory(10)->create();
+        Video::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
